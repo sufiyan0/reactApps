f1378ca9b4a8b974f25df2b5da1452626267fd30
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2024-01-19 01:07:29 +0500

    done

--- color-changing-app/src/App.jsx
+++ color-changing-app/src/App.jsx
@@ -1,14 +1,55 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [coler, setColor] = useState("black");
 
   return (
     <>
-      test
+      <div
+        className="flex flex-col  min-h-screen"
+        style={{ backgroundColor: coler }}
+      >
+        <div className="bg-gray-500  items-center  absolute rounded-2xl bottom-10 flex flex-wrap justify-center m-5 ">
+          <button
+            className="bg-yellow-300 p-2 m-4 rounded-md text-2xl font-semibold"
+            onClick={() => setColor("yellow")}
+          >
+            yello
+          </button>
+          <button
+            className="bg-green-500 p-2 m-4 rounded-md text-2xl font-semibold"
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="bg-red-500 p-2 m-4 rounded-md text-2xl font-semibold"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="bg-blue-500 p-2 m-4 rounded-md text-2xl font-semibold"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="bg-orange-500 p-2 m-4 rounded-md text-2xl font-semibold"
+            onClick={() => setColor("orange")}
+          >
+            orange
+          </button>
+          <button
+            className="bg-white p-2 m-4 rounded-md text-2xl font-semibold"
+            onClick={() => setColor("white")}
+          >
+            white
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
